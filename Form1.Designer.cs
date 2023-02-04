0d1456cd59bfa24a34f0021aac4bd095a8732992
Gabrihellzin <bielmuaway91@gmail.com>
2023-02-04 15:29:06 -0300

    Feat: Cursor events and changes in hovering the 'no' Button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,67 +36,48 @@
             // 
             // button_yes
             // 
-            this.button_yes.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button_yes.Location = new System.Drawing.Point(51, 178);
+            resources.ApplyResources(this.button_yes, "button_yes");
             this.button_yes.Name = "button_yes";
-            this.button_yes.Size = new System.Drawing.Size(80, 43);
-            this.button_yes.TabIndex = 0;
-            this.button_yes.Text = "Yes";
             this.button_yes.UseVisualStyleBackColor = true;
             // 
             // button_no
             // 
-            this.button_no.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button_no.Location = new System.Drawing.Point(310, 186);
+            this.button_no.Cursor = System.Windows.Forms.Cursors.No;
+            resources.ApplyResources(this.button_no, "button_no");
             this.button_no.Name = "button_no";
-            this.button_no.Size = new System.Drawing.Size(55, 27);
-            this.button_no.TabIndex = 0;
-            this.button_no.Text = "No";
             this.button_no.UseVisualStyleBackColor = true;
+            this.button_no.Click += new System.EventHandler(this.button_no_MouseEnter);
+            this.button_no.MouseEnter += new System.EventHandler(this.button_no_MouseEnter);
+            this.button_no.MouseHover += new System.EventHandler(this.button_no_MouseEnter);
             // 
             // box_question
             // 
-            this.box_question.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.box_question, "box_question");
             this.box_question.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.box_question.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.box_question.Location = new System.Drawing.Point(12, 34);
-            this.box_question.Multiline = true;
             this.box_question.Name = "box_question";
             this.box_question.ReadOnly = true;
-            this.box_question.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.box_question.ShortcutsEnabled = false;
-            this.box_question.Size = new System.Drawing.Size(410, 54);
-            this.box_question.TabIndex = 2;
             this.box_question.TabStop = false;
-            this.box_question.Text = "Do you want to be my girlfriend?";
-            this.box_question.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Surprise
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 261);
-            this.Controls.Add(this.box_question);
             this.Controls.Add(this.button_no);
+            this.Controls.Add(this.box_question);
             this.Controls.Add(this.button_yes);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Surprise";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Surprise";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private Button button_yes;
         private Button button_no;
         private TextBox box_question;
+        public Button button_yes;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,76 @@
+using System.Runtime.InteropServices;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace SurpriseForGirlfriend
 {
+
     public partial class Surprise : Form
     {
         public Surprise()
         {
             InitializeComponent();
+            Choice = 0;
+        }
+
+        public static int Choice { get; set; }
+
+
+        public static int[] ReturnRandomPosition(System.Windows.Forms.Button button_yes, System.Windows.Forms.TextBox box_question)
+        {
+            Random rand = new Random();
+            // Window Size - 50px 
+            int wWidth = 450 - 50;
+            int wHeight = 300 - 50;
+            int[] pos = new int[2];
+            int x = 0;
+            int y = 0;
+            bool notcollided = false;
+            // Button Properties
+            int buttonWidthStart = button_yes.Left;
+            int buttonWidthEnd = button_yes.Left + button_yes.Size.Width;
+            int buttonHeightStart = button_yes.Top;
+            int buttonHeightEnd = button_yes.Top + button_yes.Size.Height;
+            // Box Properties
+            int boxWidthStart = box_question.Left;
+            int boxWidthEnd = box_question.Left + box_question.Size.Width;
+            int boxHeightStart = box_question.Top;
+            int boxHeightEnd = box_question.Top + box_question.Size.Height;
+
+
+            // Start Generating and checking collide
+            while (notcollided == false)
+            { 
+                //Generate random number between 10 and the size of the window
+                x = rand.Next(10,wWidth);
+                y = rand.Next(10, wHeight);
+
+                //Checking Collision with button
+                if (!((x >= buttonWidthStart && x <= buttonWidthEnd) && (y >= buttonHeightStart && y <= buttonHeightEnd) ))
+                {
+                    // Checking Collision with box
+                    if (!((x >= boxWidthStart && x <= boxWidthEnd) && (y >= boxHeightStart && y <= boxHeightEnd)))
+                    {
+                        notcollided = true;
+                    }
+                }
+
+            }
+
+            //Put generated numbers in array and return it
+            pos[0] = x;
+            pos[1] = y;
+            return pos;
+        }
+
+
+        //When mouse hover the "no" button
+        private void button_no_MouseEnter(object sender, EventArgs e)
+        {
+            int[] Pos = ReturnRandomPosition(button_yes, box_question);
+
+            button_no.Location = new Point(Pos[0], Pos[1]);
+
+
         }
     }
 }
