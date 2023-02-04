8a0bd19784f01e31fa6f277c4144a1693ded9c18
Gabrihellzin <bielmuaway91@gmail.com>
2023-02-04 13:29:18 -0300

    feat: main window properties

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SurpriseForGirlfriend
 {
-    partial class Form1
+    partial class Surprise
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,10 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Surprise));
+            this.SuspendLayout();
+            // 
+            // Surprise
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(434, 261);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Surprise";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Surprise";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Form1.cs
+++ Form1.cs
@@ -1,8 +1,8 @@
 namespace SurpriseForGirlfriend
 {
-    public partial class Form1 : Form
+    public partial class Surprise : Form
     {
-        public Form1()
+        public Surprise()
         {
             InitializeComponent();
         }
